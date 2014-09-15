851e468184de87249ab3b1b25d7f8907cefd0e9a
Gorkarvien <gorkarvien@live.fr>
2014-09-15 18:17:11 +0200

    add commented utility code
    add a popup windows telling were is the base file search path

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include "engine/GameEngine.h"
 #include <SFML/Graphics.hpp>
+//#include <Windows.h>
 
 int main(int argc, char **argv)
 {
 //ajouter gestion d'argument
+
+// be sure to #include <Windows.h> .. at least temporarily
+  /*  char path[MAX_PATH];
+    GetCurrentDirectoryA(MAX_PATH,path);
+    MessageBoxA(NULL,path,"Current Directory",MB_OK);
+	*/
 
 	engine::GameEngine gameEngine;
 	gameEngine.initialization();
