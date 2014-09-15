69a5fd75cd71a078da86ee16ba71084759325acc
Gorkarvien <gorkarvien@live.fr>
2014-09-15 18:13:48 +0200

    fix project generator script
    you need to use sfml library in debug or release when building the game
    in debug or release (no mix)

--- premake/premake4.lua
+++ premake/premake4.lua
@@ -15,17 +15,29 @@ solution "theGame"
 
 		libdirs {"../lib/SFML-2.1/build/lib"}
 		-- link ? we install sfml on linux host ?
-		links {	"sfml-audio", 
-		       	"sfml-network", 
-		       	"sfml-graphics", 
-		       	"sfml-window",--sfmlwindows and sfmlsystem in last position of sfml libs for gcc 
-			"sfml-system"}
+		
 
+		
 
 		configuration "Debug"
 			defines { "DEBUG" }
 			flags { "Symbols" }
+			links {	"sfml-audio-d", 
+					"sfml-network-d", 
+					"sfml-graphics-d", 
+					"sfml-window-d",--sfmlwindows and sfmlsystem in last position of sfml libs for gcc 
+					"sfml-system-d"}
+			
+			--if os.is("windows") then
+				-- Enables console for the TEST project on Windows
+				--linkoptions { "/SUBSYSTEM:CONSOLE" }
+			--end
 
 		configuration "Release"
 			defines { "NDEBUG" }
 			flags { "Optimize" }
+			links {	"sfml-audio", 
+					"sfml-network", 
+					"sfml-graphics", 
+					"sfml-window",--sfmlwindows and sfmlsystem in last position of sfml libs for gcc 
+					"sfml-system"}
